71eefa3ef28f8028461188531ecdb14c2410951a
Peter Muller <peterianmuller@gmail.com>
2017-03-13 10:45:14 -0700

    game function returns board with a 3 x 3 matrix of integer 0

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 module.exports = {
   game: function() {
-
+    //create board
+    var board = [[0,0,0],[0,0,0],[0,0,0]];
+    return board;
   }  
      
 };
--- test/test.js
+++ test/test.js
@@ -10,5 +10,15 @@ describe('intitial tests', () => {
     var gameFunction = game.game;
     gameFunction.should.be.a('function'); 
   });
+  it('game should return a 3 x 3 matrix of arrays', ()=>{
+    var gameFunction = game.game;
+    var gameFunctionResult = gameFunction();
+
+    gameFunctionResult.length.should.equal(3);
+    for (var i = 0; i < gameFunctionResult.length; i++) {
+      gameFunctionResult[i].length.should.equal(3);
+    }
+
+  });
 
 })
