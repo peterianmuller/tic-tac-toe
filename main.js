c86b8dd7645b88b890e277e81fd62d4ae1a0c01e
Peter Muller <peterianmuller@gmail.com>
2017-03-13 12:00:16 -0700

    Simplified makeMkove function

--- main.js
+++ main.js
@@ -12,49 +12,47 @@ module.exports = {
   },       
 };
 
+
+
+//add validation functions to ensure that other moves are not allowed
+module.exports.Game.validateMove = function(board, player, move) {
+    
+    var charToCheck;
+
+    //Find out which character to check for
+    
+      // if (player === 'player1') {
+      //   charToCheck = 'O';
+      // } else {
+      //   charToCheck = 'X';
+      // }
+    
+    //iterate over board 
+    if (board) {
+      // check if piece already exists
+      if (board[move[0]][move[1]]) {
+        return false;
+      }
+    }
+
+    return true;             
+}
+
 // add play functions to board
 // player1 and player2 both make moves. 
   // Player 1 places 'X' and player 2 places 'O'
 module.exports.Game.prototype.makeMove = function(board, player, move) {
   board = board || new module.exports.Game();
-  
-  //check if player1
-  //if so check if any moves are not permitted
-    if (player === 'player1') {
-      //player one makes a move
-      this.board[move[0]][move[1]] = 'X';
-    } else {
-      this.board[move[0]][move[1]] = 'O';
-    }    
+  var piece = player === 'player1' ? 'X' : 'O';
 
+  //need to figure out how to reference validateMove
+  
+  this.board[move[0]][move[1]] = piece;
+   
   return this.board;
 };
 
-//add validation functions to ensure that other moves are not allowed
-module.exports.Game.validateMove = function(board, player, move) {
 
-    var charToCheck;
-
-    //Find out which character to check for
-    if (player) {
-      if (player === player1) {
-        charToCheck = 'O';
-      } else {
-        charToCheck = 'X';
-      }
-    }  
-    
-    //iterate over board 
-    if (board) {
-      for ( var i = 0; i < board.length; i++) {
-        for ( var j = 0; j < board[i].length; j++) {
-        //check if charToCheck disallows move
-        }
-      }
-    }  
-
-    return true;             
-}
 
 
 
